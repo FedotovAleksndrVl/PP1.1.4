3a977ee1fcb676c448a20c3f11319724e1893d3d
20 Соток <FedotovAlexanderVl@mail.ru>
2024-09-18 15:31:44 +0300

    18/ на работе

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,42 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
+    private final UserDao instanceDAO = new UserDaoJDBCImpl();
 
+    public void createUsersTable() {
+        instanceDAO.createUsersTable();
+    }
+
+    @Override
+    public String toString() {
+        return "UserServiceImpl{" +
+                "instanceDAO=" + instanceDAO +
+                '}';
     }
 
     public void dropUsersTable() {
-
+        instanceDAO.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        instanceDAO.saveUser(name,lastName, age);
     }
 
     public void removeUserById(long id) {
-
+        instanceDAO.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return null;
+        return instanceDAO.getAllUsers();
     }
 
     public void cleanUsersTable() {
-
+        instanceDAO.createUsersTable();
     }
 }
